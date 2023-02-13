36d7e32c1cb58e06cb039df7e48899618512633e
Ahmed Anas <ahmed.a.tamim@gmail.com>
2023-02-13 12:58:49 +0000

    Added hook

--- diam/sm/sm.go
+++ diam/sm/sm.go
@@ -13,6 +13,8 @@ import (
 	"github.com/fiorix/go-diameter/v4/diam/sm/smpeer"
 )
 
+type MessageHandler = func(diam.Conn, *diam.Message)
+
 // SupportedApp holds properties of each locally supported App
 type SupportedApp struct {
 	ID      uint32
@@ -68,6 +70,9 @@ type Settings struct {
 	//
 	// Deprecated: HostIPAddress is depreciated, use HostIPAddresses instead
 	HostIPAddress datatype.Address
+
+	// CerHook is called before processing of CER's. Modification can be made here
+	CerHook MessageHandler
 }
 
 var (
