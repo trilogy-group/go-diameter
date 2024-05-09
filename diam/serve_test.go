68784de14dd8e9e2a8186b04d813782c46c2871e
Ahmed Anas <ahmed.anas@trilogy.com>
2024-05-09 13:48:56 +0300

    Revert "CE: Add/Modify Capabilities in Diameter Adapter"
    This reverts commit 47fd2a51e1ddffa264364fa9bf506c318fe9b3d9.

--- diam/serve_test.go
+++ diam/serve_test.go
@@ -52,11 +52,6 @@ func TestCapabilitiesExchange(t *testing.T) {
 func TestCapabilitiesExchangeTLS(t *testing.T) {
 	errc := make(chan error, 1)
 
-	cert, err := tls.LoadX509KeyPair("testdata/example-cert.pem", "testdata/example-key.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	smux := diam.NewServeMux()
 	smux.Handle("CER", handleCER(errc, true))
 
@@ -65,8 +60,8 @@ func TestCapabilitiesExchangeTLS(t *testing.T) {
 	srv.Config.ReadTimeout = tm
 	srv.Config.WriteTimeout = tm
 	srv.TLS = &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		MinVersion:   tls.VersionTLS13,
+		MinVersion: tls.VersionTLS10,
+		MaxVersion: tls.VersionTLS10,
 	}
 	srv.StartTLS()
 	time.Sleep(time.Millisecond * 10) // let srv start
@@ -75,7 +70,7 @@ func TestCapabilitiesExchangeTLS(t *testing.T) {
 	cmux := diam.NewServeMux()
 	cmux.Handle("CEA", handleCEA(errc, wait))
 
-	cli, err := diam.DialTLS(srv.Addr, "testdata/example-cert.pem", "testdata/example-key.pem", cmux, nil)
+	cli, err := diam.DialTLS(srv.Addr, "", "", cmux, nil)
 	if err != nil {
 		t.Fatalf("diam.DialTLS Error: %v", err)
 	}
