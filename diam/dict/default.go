4d5fd60291a01dec0bd9929dd8f951ef40747a9e
Huzefa Tahir <huzefa.tahir@gmail.com>
2025-10-04 17:45:11 +0500

    fixed AVP

--- diam/dict/default.go
+++ diam/dict/default.go
@@ -1669,9 +1669,9 @@ var gxcreditcontrolXML = `<?xml version="1.0" encoding="UTF-8"?>
 			</data>
 		</avp>
 
-        <avp name="Content-Version" code="552" vendor-id="10415" must="V" may-encrypt="Y">
+        <avp name="Content-Version" code="552" must="V" may="P" must-not="M" may-encrypt="Y" vendor-id="10415">
 			<!-- 3GPP TS 29.214 5.3.49 -->
-			<data type="Unsigned32"/>
+			<data type="Unsigned64"/>
 		</avp>
 
     </application>
